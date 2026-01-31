586d4c5bee08a445e1696be8e5f392d1e11e69a5
Krzysztof Łyszczarz <64140326+MrKrzychu46@users.noreply.github.com>
2026-01-31 12:24:23 +0100

    poprawki

--- firebase.ts
+++ firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "***",
-    authDomain: "***",
-    projectId: "***",
-    storageBucket: "***",
-    messagingSenderId: "***",
-    appId: "***",
-    measurementId: "***"
+    apiKey: "AIzaSyBhL-GfqIUGrh8SGLeHPrMw4EE3905upEE",
+    authDomain: "monopolibank-8b65a.firebaseapp.com",
+    projectId: "monopolibank-8b65a",
+    storageBucket: "monopolibank-8b65a.firebasestorage.app",
+    messagingSenderId: "503826484304",
+    appId: "1:503826484304:web:dc2fedeca52240d92ed555",
+    measurementId: "G-PBWJ2F70NR"
 };
 
 const app = initializeApp(firebaseConfig);
